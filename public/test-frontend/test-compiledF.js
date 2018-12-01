0647a23dfa37f52760f855b061cd87b33d4d83fc
julianamtyler <julianam.tyler@gmail.com>
2018-12-01 11:42:10 -0500

    edited front end test

--- public/test-frontend/test-compiledF.js
+++ public/test-frontend/test-compiledF.js
@@ -1,46 +1,23 @@
-const expect = require('chai').expect;
+const assert = require('chai').expect;
 
 describe('travelList', function(){
-  describe('coldTypeList', function() {
-    it('should return', function(){
-      expect(coldList([{item:'boots',item:'jacket'}])).to.equal([{item:'boots',item:'jacket'}])
+
+    it('should return an object', function(){
+      assert.equal({item:'boots',item:'jacket'})
     });
-    it('should return items when four selections are made', function() {
-      expect(obj).to.have.nested.property();
-      expect(newList).to.equal()
-      expect(newList).to.have.length(4);
-    });
-
-  //   it('should give false if >= 3 buttons selected', function() {
-  //     expect(travelList()).to.throw(error);
-  //   });
-
-  //   it('should return an array of objects when 4 are selected', function() {
-  //     expect(travelList(result)).to.have.deep.property([[i].title, [i].items]);
-  //   });
-  });
-
-  describe('hotTypeList', function() {
-  });
-  
 });
 
 //DOM testing
 describe('travelList', function () {
-
-  beforeEach(function (){
-    display = none;
-  })
-   
-
   
-    it('should displays pre-compiled list after selection 4 icons', function () {
+    it('should displays pre-compiled list after selection 4 icons & list is named', function () {
        
         $('#weather').trigger('click');
         $('#type').trigger('click'); 
         $('#location').trigger('click');
-        $('#travelLevel').trigger('click');
-  
-      expect($('#content').text()).to.equal('Paris');
+        $('#travelLevel').trigger('click')
+        .prompt('input').trigger('click')//submit or cancel
+
+      expect($('#content').text()).to.equal();
       });
     });
