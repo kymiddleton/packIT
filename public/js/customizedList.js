9ce4abacc56a0c2ced9bab84e748608e2d9925fc
Pawan Jada <pawanjada3@gmail.com>
2018-12-05 22:02:14 -0500

    UI front end work

--- public/js/customizedList.js
+++ public/js/customizedList.js
@@ -1,1 +1,18 @@
 // Customized pre-compiled packing list
+
+$(".image").on("click", function (event) {
+    let imageName = $(this).attr("class");
+    console.log(imageName)
+    if (imageName.split(" ")[2] == "gold") {
+        $(this).removeClass('gold')
+        $(this).addClass('white')
+        $(`#${imageName.split(" ")[1]} gold`).hide();
+        $(`#${imageName.split(" ")[1]}_white`).show();
+    } else {
+        $(this).removeClass('white')
+        $(this).addClass('gold')
+        $(`#${imageName.split(" ")[1]}_gold`).show();
+        $(`#${imageName.split(" ")[1]}_white`).hide();
+    }
+    $(this).css("color", "gold");
+});
--- public/test-frontend/test-customF.js
+++ public/test-frontend/test-customF.js
@@ -68,3 +68,97 @@ describe('decrement', function () {
     });
 
 });
+
+// -------------------------------------------------
+
+//shows modal container
+const showModal = function (e) {
+    e.preventDefault();
+    $('.modal-container').show();
+}
+//hides modal container
+const hideModal = function (e) {
+    e.preventDefault();
+    $('.modal-container').hide();
+}
+
+const showPage2 = function (e) {
+    e.preventDefault();
+    $('.pagetwo').show();
+}
+
+const hidePage2 = function (e) {
+    e.preventDefault();
+    $('.pagetwo').hide();
+}
+$('gotrip').on('click', hideModal)
+$('gotrip').on('click', showPage2)
+$('notrip').on('click', hideModal)
+
+
+//saves value of the textarea when button is pressed, and hides modal container
+const saveTrip = function (e) {
+    e.preventDefault();
+    const inputTrip = $('#triplocation').val();
+    $('#triplocation').val('')
+    hideModal(e);
+    const tripData = {
+        tripName: inputTrip,
+        tripList: [],
+    }
+    $.post('/api/trip', tripData)
+        .then(function (data) {
+            render(data)
+        })
+
+}
+
+$('.main-feed').on('click', '#delete', function () {
+    const id = $(this).data('id')
+    $.ajax({
+            method: 'delete',
+            url: `/api/Trip/${id}`
+
+        })
+        .then(getTrip());
+})
+//targets Trip button on homepage to show modal window when pressed
+$('#newTrip').on('click', showModal);
+//closes modal window when x button is pressed on that window
+$('.close-modal').on('click', hideModal);
+//closes modal window when Trip is made
+$('#Trippost').on('click', saveTrip);
+
+
+//get route, that pulls the Trip data from database, and renders it to the page as individual Trips. Creates a timeline of Trips by looping through all the Trips in database.
+const getTrip = function () {
+    $('.center-feed').empty()
+    $.get('/api/Trip')
+        .then(function (serverData) {
+            for (let i = 0; i < serverData.length; i++) {
+                render(serverData[i]);
+            }
+        })
+}
+getTrip();
+
+
+$(".image").on("click", function () {
+    $(this).css("background-color", "gold");
+});
+// let count = 0
+
+// const counter = function () {
+//     count = count + 1;
+//     return count;
+// }
+// const hideform = function () {
+//     if (count === 4) {
+//         $('#form').hide();
+//         $('#modal').show();
+//     }
+// }
+// $('.weather').on('click', counter)
+// $('.travel').on('click', counter)
+// $('.destination').on('click', counter)
+// $('.expense').on('click', counter)
