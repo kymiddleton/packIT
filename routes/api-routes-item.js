a882fe9eed7f647fb3670a0b8774e1786efc1b30
Choleslaw <41842273+kymiddleton@users.noreply.github.com>
2018-12-06 18:05:39 -0500

    modifications to get api routes working

--- routes/api-routes-item.js
+++ routes/api-routes-item.js
@@ -5,13 +5,8 @@ const db = require('../models');
 module.exports = function (app) {
 
     // GET request: Route for retrieving Packing List Items from the database.
-    app.get('/api/item-schema/:weather/:packing/:destination/:travel', function (req, res) { //Works
-        db.packingItem.find({
-            weather: req.params.weather,
-            packing: req.params.packing,
-            destination: req.params.destination,
-            travel: req.params.travel
-        })
+    app.get('/api/item-schema', function (req, res) { //works
+        db.packingItem.find({})
             .then(function (dbpackingItem) {
                 res.json(dbpackingItem);
             })
@@ -22,7 +17,7 @@ module.exports = function (app) {
 
     // POST request: Route for creating new Packing List Items in the database.
     app.post('/api/item-schema', function (req, res) { //Works
-        console.log('------Adding Link in mongo');
+        console.log('------Adding Item in mongo');
         db.packingItem.create(req.body)
             .then(function (dbpackingItem) {
                 res.json(dbpackingItem);
@@ -34,9 +29,18 @@ module.exports = function (app) {
 
 
     // PUT request: Route for updating Packing List content / saving updates 
-    app.put('/api/item-schema', function (req, res) {
-        console.log('----> updating <----');
-        db.packingIem.findOneAndUpdate({ _id: req.body.id })
+    // app.post('/api/update/item-schema', function (req, res) {
+    app.put('/api/item-schema', function (req, res) { // NOT working
+        console.log('----> updating item <----');
+        db.packingItem.findOneAndUpdate({ _id: req.body.id }, 
+            { $set: {
+                item: req.body.item,
+                category: req.body.category,
+                weather: req.body.weather,
+                packing: req.body.packing,
+                destination: req.body.destination,
+                travel: req.body.travel
+            }})
             .then(function (dbpackingItem) {
                 res.json(dbpackingItem);
             })
@@ -46,9 +50,11 @@ module.exports = function (app) {
     });
 
     // DELETE request: Deletes Packing List content
-    app.delete('/api/item-schema/:packingItem_id', function (req, res) {
-        console.log('--------deleting--------');
-        db.packingItem.findByIdAndRemove(req.params.packingItem_id, function (err, packingItem) {
+    // app.post('/api/delete/item-schema/:packingItem_id', function (req, res) {
+    app.delete('/api/item-schema/:packingItem_id', function (req, res) { //NOT working
+        console.log('--------deleting item --------');
+        db.packingItem.findByIdAndRemove(req.body.id, function (err, packingItem) {
+        // db.packingItem.findByIdAndRemove(req.params.packingItem_id, function (err, packingItem) {
             if (err) return res.status(500).send(err);
             // We'll create a simple object to send back with a message and the id of the document that was removed
             const response = {
--- routes/api-routes-user.js
+++ routes/api-routes-user.js
@@ -5,8 +5,8 @@ const db = require('../models');
 module.exports = function (app) {
 
     // GET request: Route for retrieving users from the database.
-    app.get('/api/user-schema', function (req, res) { 
-        console.log('----retrieving----');
+    app.get('/api/user-schema', function (req, res) { //Works
+        console.log('----retrieving user----');
         db.user.find({})
             .then(function (dbuser) {
                 console.log(dbuser)
@@ -18,8 +18,8 @@ module.exports = function (app) {
     });
 
     // POST request: Route for creating new users in the database.
-    app.post('/api/user-schema', function (req, res) {  
-        console.log('------Adding to Mongo');
+    app.post('/api/user-schema', function (req, res) {  //Working
+        console.log('------Adding User to Mongo');
         db.user.create(req.body)
             .then(function (dbuser) {
                 res.json(dbuser);
@@ -30,8 +30,8 @@ module.exports = function (app) {
     });
 
     // PUT request: Route for updating users
-    app.put('/api/user-schema', function (req, res) { 
-        console.log('----> updating <----');
+    app.put('/api/user-schema', function (req, res) { // NOT working
+        console.log('----> updating user <----');
         db.user.findOneAndUpdate({ _id: req.body.id }, { $set: { password: req.body.password, email: req.body.email, userName: req.body.userName } })
             .then(function (dbuser) {
                 res.json(dbuser);
@@ -42,9 +42,9 @@ module.exports = function (app) {
     });
 
     // DELETE request: Deletes user content
-    app.delete('/api/user-schema/:id', function (req, res) { 
-        console.log('------deleting------');
-        db.user.findOneAndRemove(req.params.user_id, function (err, links) {
+    app.delete('/api/user-schema/:id', function (req, res) { // NOT working
+        console.log('------deleting user------');
+        db.user.findOneAndDelete(req.params.user_id, function (err, links) {
             if (err) return res.status(500).send(err);
             // We'll create a simple object to send back with a message and the id of the document that was removed
             const response = {
