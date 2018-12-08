02fdbdc7011fe041d33bc2cf34a41200b5b01c08
Pawan Jada <pawanjada3@gmail.com>
2018-12-08 11:32:41 -0500

    testing image changes

--- routes/api-routes-item.js
+++ routes/api-routes-item.js
@@ -6,19 +6,25 @@ module.exports = function (app) {
 
     // GET request: Route for retrieving Packing List Items from the database.
     app.get('/api/item-schema/:weather/:packing/:destination/:travel', function (req, res) { //Works
-<<<<<<< HEAD
-=======
-        
+
         console.log(req.params)
         // res.send(true)
         // console.log(req.body)
->>>>>>> c8d0a42e9b77d72698322f9dff58217cedb6aacf
-        db.packingItem.find({$and :[
-           {weather: req.params.weather},
-            {packing: req.params.packing},
-            {destination: req.params.destination},
-            {travel: req.params.travel}
-        ]})
+        db.packingItem.find({
+                $and: [{
+                        weather: req.params.weather
+                    },
+                    {
+                        packing: req.params.packing
+                    },
+                    {
+                        destination: req.params.destination
+                    },
+                    {
+                        travel: req.params.travel
+                    }
+                ]
+            })
             .then(function (dbpackingItem) {
                 console.log(dbpackingItem)
                 res.json(dbpackingItem);
@@ -27,7 +33,7 @@ module.exports = function (app) {
                 res.json(err);
             });
     });
-    
+
     // POST request: Route for creating new Packing List Items in the database.
     app.post('/api/item-schema', function (req, res) { //Works
         console.log('------Adding Item in mongo');
@@ -45,15 +51,18 @@ module.exports = function (app) {
     // app.post('/api/update/item-schema', function (req, res) {
     app.put('/api/item-schema', function (req, res) { // NOT working
         console.log('----> updating item <----');
-        db.packingItem.findOneAndUpdate({ _id: req.body.id }, 
-            { $set: {
-                item: req.body.item,
-                category: req.body.category,
-                weather: req.body.weather,
-                packing: req.body.packing,
-                destination: req.body.destination,
-                travel: req.body.travel
-            }})
+        db.packingItem.findOneAndUpdate({
+                _id: req.body.id
+            }, {
+                $set: {
+                    item: req.body.item,
+                    category: req.body.category,
+                    weather: req.body.weather,
+                    packing: req.body.packing,
+                    destination: req.body.destination,
+                    travel: req.body.travel
+                }
+            })
             .then(function (dbpackingItem) {
                 res.json(dbpackingItem);
             })
@@ -67,7 +76,7 @@ module.exports = function (app) {
     app.delete('/api/item-schema/:packingItem_id', function (req, res) { //NOT working
         console.log('--------deleting item --------');
         db.packingItem.findByIdAndRemove(req.body.id, function (err, packingItem) {
-        // db.packingItem.findByIdAndRemove(req.params.packingItem_id, function (err, packingItem) {
+            // db.packingItem.findByIdAndRemove(req.params.packingItem_id, function (err, packingItem) {
             if (err) return res.status(500).send(err);
             // We'll create a simple object to send back with a message and the id of the document that was removed
             const response = {
