f9a36e43355ece26b700987c30db1455e360f494
julianamtyler <julianam.tyler@gmail.com>
2018-12-08 13:17:13 -0500

    added modal feature and we can go back and change value

--- public/js/compiled.js
+++ public/js/compiled.js
@@ -27,56 +27,47 @@ $(document).ready(function () {
             $('.image').addClass('hide');
             $('.modal').removeClass('hide');
         }
-        // const cancel = function () {
-        //     $('.modal').addClass('hide')
-        //     $('.image').removeClass('hide');
-        //     $('.image').on('click',selections)
-        // }
-        // $('#notrip').on('click', cancel);
 
     }
     $('.image').on('click', selections);
 
 
     $('#gotrip').on('click', function () {
-        // console.log('clicked trip')
-        // console.log(weather + packing + destination + travel)
-        $.ajax({
-            url: `/api/item-schema/${weather}/${packing}/${destination}/${travel}`, method: 'GET'
-        })
-            .then(function (data) {
-                $()
-                console.log(data)
-                data.map(e => {
-                    for (let i = 0; i < e.data.length; i++) { 
-                        console.log(e.data.item)
-                     }
-                    // for (let i = 0; i < e.data.length; i++) { 
-                    //     if (e.data[i].category === 'clothing') {
-                    //         $('.clothing').append($(`<li><<input class="checkbox" type="checkbox"/>${e.data[i].item}</li>`))
-                    //     } else if (e.data[i].category === 'footwear') {
-                    //         $('.footwear').append($(`<li><<input class="checkbox" type="checkbox"/>${e.data[i].item}</li>`))
-                    //     } else if (e.data[i].category === 'personal') {
-                    //         $('.personal').append(`<li><<input class="checkbox" type="checkbox"/>${e.data[i].item}</li>`)
-                    //     } else if (e.data[i].category === 'documents') {
-                    //         $('.documents').append(`<li><<input class="checkbox" type="checkbox"/>${e.data[i].item}</li>`)
-                    //     } else if (e.data[i].category === 'gadgets') {
-                    //         $('.gadgets').append(`<li><<input class="checkbox" type="checkbox"/>${e.data[i].item}</li>`)
-                    //     } else if (e.data[i].category === 'miscelleneous') {
-                    //         $('.miscelleneous').append(`<li><<input class="checkbox" type="checkbox"/>${e.data[i].item}</li>`)
-                    //     }
-                    // }
-                })
-
+        // console.log(data)	        
+        console.log(weather + packing + destination + travel)
+            $.ajax({
+                url: `/api/item-schema/${weather}/${packing}/${destination}/${travel}`, method: 'GET'
             })
+                .then(function (data) {
+                    console.log(data)
+                    data.map(e => {
+                        for (let i = 0; i < data.length; i++) {
+                            if (e.data[i].category === 'clothing') {
+                                $('.clothing').append($(`<li><<input class="checkbox" type="checkbox"/>${e.data[i].item}</li>`))
+                            } else if (e.data[i].category === 'footwear') {
+                                $('.footwear').append($(`<li><<input class="checkbox" type="checkbox"/>${e.data[i].item}</li>`))
+                            } else if (e.data[i].category === 'personal') {
+                                $('.personal').append(`<li><<input class="checkbox" type="checkbox"/>${e.data[i].item}</li>`)
+                            } else if (e.data[i].category === 'documents') {
+                                $('.documents').append(`<li><<input class="checkbox" type="checkbox"/>${e.data[i].item}</li>`)
+                            } else if (e.data[i].category === 'gadgets') {
+                                $('.gadgets').append(`<li><<input class="checkbox" type="checkbox"/>${e.data[i].item}</li>`)
+                            } else if (e.data[i].category === 'miscelleneous') {
+                                $('.miscelleneous').append(`<li><<input class="checkbox" type="checkbox"/>${e.data[i].item}</li>`)
+                            }
+                        }
+                    })
+            })
+            
 
-    })
+  
     const cancel = function () {
         $('.modal').addClass('hide')
         $('.image').removeClass('hide');
-        $('.image').on('click',reselect)
+        $('.image').on('click',selections)
+        console.log(weather + destination + travel + packing)
     }
     $('#notrip').on('click', cancel);
 
-
+})
 })
--- routes/api-routes-item.js
+++ routes/api-routes-item.js
@@ -8,11 +8,11 @@ module.exports = function (app) {
     app.get('/api/item-schema/:weather/:packing/:destination/:travel', function (req, res) { //Works
         console.log(req.params)
         db.packingItem.find({$and :[
-           {weather: req.params.weather},
-            {packing: req.params.packing},
-            {destination: req.params.destination},
-            {travel: req.params.travel}
-        ]})
+            {weather:{$in : [req.params.weather]}},
+             {packing: {$in : [req.params.packing]}},
+             {destination: {$in : [req.params.destination]}},
+             {travel: {$in : [req.params.travel]}}
+         ]})
             .then(function (dbpackingItem) {
                 console.log(dbpackingItem)
                 res.json(dbpackingItem);
@@ -23,52 +23,52 @@ module.exports = function (app) {
     });
     
     // POST request: Route for creating new Packing List Items in the database.
-    app.post('/api/item-schema', function (req, res) { //Works
-        console.log('------Adding Item in mongo');
-        db.packingItem.create(req.body)
-            .then(function (dbpackingItem) {
-                res.json(dbpackingItem);
-            })
-            .catch(function (err) {
-                res.json(err);
-            });
-    });
+    // app.post('/api/item-schema', function (req, res) { //Works
+    //     console.log('------Adding Item in mongo');
+    //     db.packingItem.create(req.body)
+    //         .then(function (dbpackingItem) {
+    //             res.json(dbpackingItem);
+    //         })
+    //         .catch(function (err) {
+    //             res.json(err);
+    //         });
+    // });
 
 
-    // PUT request: Route for updating Packing List content / saving updates 
-    // app.post('/api/update/item-schema', function (req, res) {
-    app.put('/api/item-schema', function (req, res) { // NOT working
-        console.log('----> updating item <----');
-        db.packingItem.findOneAndUpdate({ _id: req.body.id }, 
-            { $set: {
-                item: req.body.item,
-                category: req.body.category,
-                weather: req.body.weather,
-                packing: req.body.packing,
-                destination: req.body.destination,
-                travel: req.body.travel
-            }})
-            .then(function (dbpackingItem) {
-                res.json(dbpackingItem);
-            })
-            .catch(function (err) {
-                res.json(err);
-            });
-    });
+    // // PUT request: Route for updating Packing List content / saving updates 
+    // // app.post('/api/update/item-schema', function (req, res) {
+    // app.put('/api/item-schema', function (req, res) { // NOT working
+    //     console.log('----> updating item <----');
+    //     db.packingItem.findOneAndUpdate({ _id: req.body.id }, 
+    //         { $set: {
+    //             item: req.body.item,
+    //             category: req.body.category,
+    //             weather: req.body.weather,
+    //             packing: req.body.packing,
+    //             destination: req.body.destination,
+    //             travel: req.body.travel
+    //         }})
+    //         .then(function (dbpackingItem) {
+    //             res.json(dbpackingItem);
+    //         })
+    //         .catch(function (err) {
+    //             res.json(err);
+    //         });
+    // });
 
-    // DELETE request: Deletes Packing List content
-    // app.post('/api/delete/item-schema/:packingItem_id', function (req, res) {
-    app.delete('/api/item-schema/:packingItem_id', function (req, res) { //NOT working
-        console.log('--------deleting item --------');
-        db.packingItem.findByIdAndRemove(req.body.id, function (err, packingItem) {
-        // db.packingItem.findByIdAndRemove(req.params.packingItem_id, function (err, packingItem) {
-            if (err) return res.status(500).send(err);
-            // We'll create a simple object to send back with a message and the id of the document that was removed
-            const response = {
-                message: "List successfully deleted",
-                id: packingItem._id
-            };
-            return res.status(200).send(response);
-        });
-    });
+    // // DELETE request: Deletes Packing List content
+    // // app.post('/api/delete/item-schema/:packingItem_id', function (req, res) {
+    // app.delete('/api/item-schema/:packingItem_id', function (req, res) { //NOT working
+    //     console.log('--------deleting item --------');
+    //     db.packingItem.findByIdAndRemove(req.body.id, function (err, packingItem) {
+    //     // db.packingItem.findByIdAndRemove(req.params.packingItem_id, function (err, packingItem) {
+    //         if (err) return res.status(500).send(err);
+    //         // We'll create a simple object to send back with a message and the id of the document that was removed
+    //         const response = {
+    //             message: "List successfully deleted",
+    //             id: packingItem._id
+    //         };
+    //         return res.status(200).send(response);
+    //     });
+    // });
 };
